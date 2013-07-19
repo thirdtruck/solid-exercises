a409320b5f468cc4a31a6908881cecf21b1fb8d5
James C. Holder <jholder@theladders.com>
2013-07-19 17:27:31 -0400

    Fulfill test for JobApplier#apply.

--- The-Single-Responsibility-Principle/java/src/main/java/com/theladders/solid/srp/JobApplier.java
+++ The-Single-Responsibility-Principle/java/src/main/java/com/theladders/solid/srp/JobApplier.java
@@ -1,6 +1,7 @@
 package com.theladders.solid.srp;
 
 import com.theladders.solid.srp.job.Job;
+import com.theladders.solid.srp.job.application.JobApplicationResult;
 import com.theladders.solid.srp.jobseeker.Jobseeker;
 
 public class JobApplier {
@@ -8,6 +9,11 @@ public class JobApplier {
   public JobApplier(ResumeRequest resumeRequest, Jobseeker jobseeker, Job job,
       String fileName) {
     // TODO Auto-generated constructor stub
+  }
+  
+  public JobApplicationResult apply()
+  {
+    return null;
   }
 
 }

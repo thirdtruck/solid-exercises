8539fabb93e0ce583e2ff08a3777f834b686c38d
James C. Holder <jholder@theladders.com>
2013-07-19 13:55:32 -0400

    Fulfill class initialization spec for HttpRequestResumeParser.

---
+++ The-Single-Responsibility-Principle/java/src/main/java/com/theladders/solid/srp/HttpRequestResumeParser.java
@@ -0,0 +1,12 @@
+package com.theladders.solid.srp;
+
+import com.theladders.solid.srp.http.HttpRequest;
+
+public class HttpRequestResumeParser
+{
+
+  public HttpRequestResumeParser(HttpRequest request)
+  {
+  }
+
+}

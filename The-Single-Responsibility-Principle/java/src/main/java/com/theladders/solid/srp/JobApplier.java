46b361ce004f3fbc63232e4795ac447eff4dc15c
James C. Holder <jholder@theladders.com>
2013-07-19 17:24:11 -0400

    Fulfill initialization test for JobApplier.

---
+++ The-Single-Responsibility-Principle/java/src/main/java/com/theladders/solid/srp/JobApplier.java
@@ -0,0 +1,13 @@
+package com.theladders.solid.srp;
+
+import com.theladders.solid.srp.job.Job;
+import com.theladders.solid.srp.jobseeker.Jobseeker;
+
+public class JobApplier {
+
+  public JobApplier(ResumeRequest resumeRequest, Jobseeker jobseeker, Job job,
+      String fileName) {
+    // TODO Auto-generated constructor stub
+  }
+
+}

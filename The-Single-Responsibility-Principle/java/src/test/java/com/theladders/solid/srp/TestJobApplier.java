770997b1bf412113117b31a63a42b7f680547039
James C. Holder <jholder@theladders.com>
2013-07-19 18:27:27 -0400

    Update tests with the to-be-required initialization method profile for JobApplier.

--- The-Single-Responsibility-Principle/java/src/test/java/com/theladders/solid/srp/TestJobApplier.java
+++ The-Single-Responsibility-Principle/java/src/test/java/com/theladders/solid/srp/TestJobApplier.java
@@ -7,10 +7,17 @@ import org.junit.*;
 import com.theladders.solid.srp.job.Job;
 import com.theladders.solid.srp.job.application.JobApplicationResult;
 import com.theladders.solid.srp.jobseeker.Jobseeker;
+import com.theladders.solid.srp.resume.ActiveResumeRepository;
+import com.theladders.solid.srp.resume.MyResumeManager;
+import com.theladders.solid.srp.resume.Resume;
+import com.theladders.solid.srp.resume.ResumeManager;
+import com.theladders.solid.srp.resume.ResumeRepository;
 
 public class TestJobApplier
 {
   private static final int JOBSEEKER_WITH_RESUME = 777;
+  
+  private ResumeController resumeController;
   
   @Test
   public void canApply()
@@ -20,7 +27,8 @@ public class TestJobApplier
     Job job = new Job(15);
     ResumeRequest resumeRequest = new ResumeRequest(false, false);
     
-    JobApplier applier = new JobApplier(resumeRequest,
+    JobApplier applier = new JobApplier(resumeController,
+                                        resumeRequest,
                                         jobseeker,
                                         job,
                                         fileName);
@@ -35,7 +43,8 @@ public class TestJobApplier
     Job job = new Job(15);
     ResumeRequest resumeRequest = new ResumeRequest(false, false);
     
-    JobApplier applier = new JobApplier(resumeRequest,
+    JobApplier applier = new JobApplier(resumeController,
+                                        resumeRequest,
                                         jobseeker,
                                         job,
                                         fileName);
@@ -47,6 +56,15 @@ public class TestJobApplier
   @Before
   public void setup()
   {
+    ResumeRepository resumeRepository = new ResumeRepository();
+    ActiveResumeRepository activeResumeRepository = new ActiveResumeRepository();
+
+    activeResumeRepository.makeActive(JOBSEEKER_WITH_RESUME, new Resume("Blammo"));
+    
+    ResumeManager resumeManager = new ResumeManager(resumeRepository);
+    MyResumeManager myResumeManager = new MyResumeManager(activeResumeRepository);
+
+    resumeController = new ResumeController(resumeManager, myResumeManager);
   }
   
 }

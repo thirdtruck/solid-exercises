1e08f87896d7e5f24eff6a39a9b69dbf09f03840
James C. Holder <jholder@theladders.com>
2013-07-22 18:04:24 -0400

    Add test for ResumePolicy#requiresCompletedResume.

--- The-Single-Responsibility-Principle/java/src/test/java/com/theladders/solid/srp/TestResumePolicy.java
+++ The-Single-Responsibility-Principle/java/src/test/java/com/theladders/solid/srp/TestResumePolicy.java
@@ -5,9 +5,13 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.theladders.solid.srp.jobseeker.Jobseeker;
+
 public class TestResumePolicy
 {
 
+  private static final int APPROVED_JOBSEEKER    = 1010;
+  
   @Before
   public void setup()
   {
@@ -17,6 +21,16 @@ public class TestResumePolicy
   public void canInitialize()
   {
     ResumePolicy policy = new ResumePolicy();
+  }
+  
+  @Test
+  public void requiresCompletedResume()
+  {
+    Jobseeker jobseeker = new Jobseeker(APPROVED_JOBSEEKER, true);
+
+    ResumePolicy policy = new ResumePolicy();
+    
+    assertTrue(policy.requiresCompletedResume(jobseeker));
   }
 
 }

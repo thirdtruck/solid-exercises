e6bdd46d3153315b9fe2d6bcf4c966fc05eb82f1
James C. Holder <jholder@theladders.com>
2013-07-22 17:59:40 -0400

    Add initial test for pending ResumePolicy class.

---
+++ The-Single-Responsibility-Principle/java/src/test/java/com/theladders/solid/srp/TestResumePolicy.java
@@ -0,0 +1,22 @@
+package com.theladders.solid.srp;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestResumePolicy
+{
+
+  @Before
+  public void setup()
+  {
+  }
+
+  @Test
+  public void canInitialize()
+  {
+    ResumePolicy policy = new ResumePolicy();
+  }
+
+}

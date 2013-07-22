0da333a74dcf5525c778477542941cbe684b8e13
James C. Holder <jholder@theladders.com>
2013-07-22 18:05:07 -0400

    Fulfill te st for ResumePolicy#requiresCompletedResume.

--- The-Single-Responsibility-Principle/java/src/main/java/com/theladders/solid/srp/ResumePolicy.java
+++ The-Single-Responsibility-Principle/java/src/main/java/com/theladders/solid/srp/ResumePolicy.java
@@ -1,6 +1,12 @@
 package com.theladders.solid.srp;
 
+import com.theladders.solid.srp.jobseeker.Jobseeker;
+
 public class ResumePolicy
 {
+
+  public boolean requiresCompletedResume(Jobseeker jobseeker) {
+    return true;
+  }
 
 }

9d18cf203a8f175c340cf78aa33159072d959587
James C. Holder <jholder@theladders.com>
2013-07-22 18:12:23 -0400

    Fulfill test for ResumePolicy#isResumeIncomplete (for ProfileStatus.INCOMPLETE).

--- The-Single-Responsibility-Principle/java/src/main/java/com/theladders/solid/srp/ResumePolicy.java
+++ The-Single-Responsibility-Principle/java/src/main/java/com/theladders/solid/srp/ResumePolicy.java
@@ -1,11 +1,16 @@
 package com.theladders.solid.srp;
 
 import com.theladders.solid.srp.jobseeker.Jobseeker;
+import com.theladders.solid.srp.jobseeker.ProfileStatus;
 
 public class ResumePolicy
 {
 
   public boolean requiresCompletedResume(Jobseeker jobseeker) {
+    return true;
+  }
+
+  public boolean isResumeIncomplete(ProfileStatus incomplete) {
     return true;
   }
 

54e1b8da37550ec7c74d7cca91fbaf76b281b76e
James C. Holder <jholder@theladders.com>
2013-07-23 10:50:25 -0400

    Add initial JUnit 4 test files.

---
+++ The-Open-Closed-Principle/java/solid-ocp/src/test/java/TestApp.java
@@ -0,0 +1,20 @@
+//import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.theladders.solid.ocp.App;
+
+
+public class TestApp {
+
+  @Before
+  public void setUp() {
+  }
+
+  @Test
+  public void test() {
+    App.main(null);
+  }
+
+}

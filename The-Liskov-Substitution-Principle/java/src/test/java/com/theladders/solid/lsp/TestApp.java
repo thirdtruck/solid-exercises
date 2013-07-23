4372b742d8e0f09d9c47e077d626b322c216ab3c
James C. Holder <jholder@theladders.com>
2013-07-23 17:21:26 -0400

    Update LSP tests to use the house code type.

--- The-Liskov-Substitution-Principle/java/src/test/java/com/theladders/solid/lsp/TestApp.java
+++ The-Liskov-Substitution-Principle/java/src/test/java/com/theladders/solid/lsp/TestApp.java
@@ -5,14 +5,17 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-public class TestApp {
+public class TestApp
+{
 
   @Before
-  public void setUp() throws Exception {
+  public void setUp() throws Exception
+  {
   }
 
   @Test
-  public void test() {
+  public void test()
+  {
     App.main(null);
   }
 

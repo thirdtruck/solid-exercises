891496e46f3e1e8512220cc7c19b9c663382e723
James C. Holder <jholder@theladders.com>
2013-07-23 13:22:28 -0400

    Update JobseekerConfidentialityProfileDao#fetchJobSeekerConfidentialityProfile to actually save profiles and fetch them, only creating new ones if they don't exist instead of every time.

--- The-Open-Closed-Principle/java/src/main/java/com/theladders/solid/ocp/jobseeker/JobseekerConfidentialityProfileDao.java
+++ The-Open-Closed-Principle/java/src/main/java/com/theladders/solid/ocp/jobseeker/JobseekerConfidentialityProfileDao.java
@@ -1,11 +1,28 @@
 package com.theladders.solid.ocp.jobseeker;
 
+import java.util.*;
+
 
 public class JobseekerConfidentialityProfileDao
 {
+  private Map<Integer, JobseekerConfidentialityProfile> profiles;
+  
+  public JobseekerConfidentialityProfileDao()
+  {
+    profiles = new HashMap<Integer, JobseekerConfidentialityProfile>();
+  }
+  
   @SuppressWarnings("unused")
   public JobseekerConfidentialityProfile fetchJobSeekerConfidentialityProfile(int id)
   {
-    return new JobseekerConfidentialityProfile();
+    JobseekerConfidentialityProfile profile = profiles.get(new Integer(id));
+    if(profile != null)
+    {
+       return profile;
+    }
+    
+    profile = new JobseekerConfidentialityProfile();
+    profiles.put(new Integer(id), profile);
+    return profile;
   }
 }

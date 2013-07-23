d2bf8ee00c6265784b8826d87cef2d97519f266f
James C. Holder <jholder@theladders.com>
2013-07-23 13:18:54 -0400

    Add categoryName property to ConfidentialPhraseCategory, as required for the ConfidentialResumeHandler tests.

--- The-Open-Closed-Principle/java/src/main/java/com/theladders/solid/ocp/resume/ConfidentialPhraseCategory.java
+++ The-Open-Closed-Principle/java/src/main/java/com/theladders/solid/ocp/resume/ConfidentialPhraseCategory.java
@@ -2,19 +2,31 @@ package com.theladders.solid.ocp.resume;
 
 public enum ConfidentialPhraseCategory
 {
-  Name(76),
-  MailingAddress(79),
-  PhoneNumber(78),
-  EmailAddress(77),
-  ContactInfo(80),
-  CompanyName(81),
-  WorkExperience(82);
+  Name(76, "Name"),
+  MailingAddress(79, "MailingAddress"),
+  PhoneNumber(78, "PhoneNumber"),
+  EmailAddress(77, "EmailAddress"),
+  ContactInfo(80, "ContactInfo"),
+  CompanyName(81, "CompanyName"),
+  WorkExperience(82, "WorkExperience");
 
   @SuppressWarnings("unused")
   private int id;
+  private String categoryName;
 
-  private ConfidentialPhraseCategory(int id)
+  private ConfidentialPhraseCategory(int id, String categoryName)
   {
     this.id = id;
+    this.categoryName = categoryName;
+  }
+  
+  public int id()
+  {
+    return this.id;
+  }
+  
+  public String categoryName()
+  {
+    return this.categoryName;
   }
 }

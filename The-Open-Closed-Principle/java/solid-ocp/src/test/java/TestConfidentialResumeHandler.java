25e9e0727c9cae9ad72c04817097136865340c67
James C. Holder <jholder@theladders.com>
2013-07-23 09:39:34 -0400

    Add initial setup code for ConfidentialResumeHandler.

--- The-Open-Closed-Principle/java/solid-ocp/src/test/java/TestConfidentialResumeHandler.java
+++ The-Open-Closed-Principle/java/solid-ocp/src/test/java/TestConfidentialResumeHandler.java
@@ -1,9 +1,23 @@
+import com.theladders.solid.ocp.jobseeker.JobseekerConfidentialityProfileDao;
+import com.theladders.solid.ocp.resume.ConfidentialResumeHandler;
+import com.theladders.solid.ocp.resume.JobseekerProfileManager;
+
 import junit.framework.TestCase;
 
 
-public class TestConfidentialResumeHandler extends TestCase {
+public class TestConfidentialResumeHandler extends TestCase
+{
 
-  protected void setUp() {
+  private JobseekerProfileManager jobseekerProfileManager;
+  private JobseekerConfidentialityProfileDao jobseekerConfidentialityProfileDao;
+  
+  private ConfidentialResumeHandler handler;
+  
+  protected void setUp()
+  {
+    jobseekerProfileManager = new JobseekerProfileManager();
+    jobseekerConfidentialityProfileDao = new JobseekerConfidentialityProfileDao();
+    
+    handler = new ConfidentialResumeHandler(jobseekerProfileManager, jobseekerConfidentialityProfileDao);
   }
-
 }

cd7008dfdb87bd8c97382c33b97a848c29da7711
James C. Holder <jholder@theladders.com>
2013-07-23 13:42:48 -0400

    Add ConfidentialPhraseCategories, which holds all the specific instances of ConfidentialPhraseCategory previously provided by the latter when it was an enum.

---
+++ The-Open-Closed-Principle/java/src/main/java/com/theladders/solid/ocp/resume/ConfidentialPhraseCategories.java
@@ -0,0 +1,12 @@
+package com.theladders.solid.ocp.resume;
+
+public class ConfidentialPhraseCategories
+{
+  public static final ConfidentialPhraseCategory Name = new ConfidentialPhraseCategory(76, "Name");
+  public static final ConfidentialPhraseCategory MailingAddress = new ConfidentialPhraseCategory(79, "MailingAddress");
+  public static final ConfidentialPhraseCategory PhoneNumber = new ConfidentialPhraseCategory(78, "PhoneNumber");
+  public static final ConfidentialPhraseCategory EmailAddress = new ConfidentialPhraseCategory(77, "EmailAddress");
+  public static final ConfidentialPhraseCategory ContactInfo = new ConfidentialPhraseCategory(80, "ContactInfo");
+  public static final ConfidentialPhraseCategory CompanyName = new ConfidentialPhraseCategory(81, "CompanyName");
+  public static final ConfidentialPhraseCategory WorkExperience = new ConfidentialPhraseCategory(82, "WorkExperience");
+}

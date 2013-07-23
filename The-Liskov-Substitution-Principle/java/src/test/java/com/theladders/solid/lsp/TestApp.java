53ac2f1ba6fe9945add760ced7aa5aef3dc34240
James C. Holder <jholder@theladders.com>
2013-07-23 17:16:15 -0400

    Add preliminary test files for The-Liskov-Substitution-Principle.

---
+++ The-Liskov-Substitution-Principle/java/src/test/java/com/theladders/solid/lsp/TestApp.java
@@ -0,0 +1,19 @@
+package com.theladders.solid.lsp;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestApp {
+
+  @Before
+  public void setUp() throws Exception {
+  }
+
+  @Test
+  public void test() {
+    fail("Not yet implemented");
+  }
+
+}

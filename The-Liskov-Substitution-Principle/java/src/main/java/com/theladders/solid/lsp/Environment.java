d09b391c6e63617be74ff3057a66651e4f735a0a
James C. Holder <jholder@theladders.com>
2013-07-24 09:22:56 -0400

    Removed unused custom constructor for Environment and its extraneous private variable.

--- The-Liskov-Substitution-Principle/java/src/main/java/com/theladders/solid/lsp/Environment.java
+++ The-Liskov-Substitution-Principle/java/src/main/java/com/theladders/solid/lsp/Environment.java
@@ -4,11 +4,5 @@ import java.util.HashMap;
 
 public class Environment extends HashMap<Object, Object>
 {
-  public static final String KEY_EMAIL_DOMAIN = "emaildomain";
-
-  public Environment()
-  {
-    super();
-  }
 
 }

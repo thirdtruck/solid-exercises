b90acc3e467326924441bbe3bebb94e654c85f1c
James C. Holder <jholder@theladders.com>
2013-07-24 17:15:12 -0400

    Add AccessibilityInfo interface.

---
+++ The-Interface-Segregation-Principle/java/src/main/java/com/theladders/solid/isp/oldjob/fulldata/AccessibilityInfo.java
@@ -0,0 +1,9 @@
+package com.theladders.solid.isp.oldjob.fulldata;
+
+public interface AccessibilityInfo {
+  public boolean isAnonymous();
+  
+  public boolean isConfidential();
+  
+  public boolean isExclusive();
+}

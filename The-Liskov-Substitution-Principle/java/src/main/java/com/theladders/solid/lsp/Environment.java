8b0a60f2b91c594285137694afa50f7606fa1f86
James C. Holder <jholder@theladders.com>
2013-07-24 09:25:06 -0400

    Made Environment a subclass of HashMap<String, String> instead of HashMap<Object, Object>, as it had expected to have and is only supplied Strings, preventing the storage of mismatched Objects.

--- The-Liskov-Substitution-Principle/java/src/main/java/com/theladders/solid/lsp/Environment.java
+++ The-Liskov-Substitution-Principle/java/src/main/java/com/theladders/solid/lsp/Environment.java
@@ -2,7 +2,7 @@ package com.theladders.solid.lsp;
 
 import java.util.HashMap;
 
-public class Environment extends HashMap<Object, Object>
+public class Environment extends HashMap<String, String>
 {
 
 }

9b2142527415d1f9b36b73c657c1d40eb9d4c690
James C. Holder <jholder@theladders.com>
2013-07-24 13:49:37 -0400

    Preliminary test setup for The-Interface-Segregation-Principle and JobInfoComposite, the test implementation of the abstract JobImpl.

---
+++ The-Interface-Segregation-Principle/java/src/test/java/TestJobInfoComposite.java
@@ -0,0 +1,27 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.theladders.solid.isp.oldjob.JobImpl;
+import com.theladders.solid.isp.oldjob.JobInfoComposite;
+
+
+public class TestJobInfoComposite
+{
+  
+  private JobInfoComposite composite;
+
+  @Before
+  public void setUp()
+  {
+    composite = new JobInfoComposite();
+  }
+
+  @Test
+  public void test()
+  {
+    fail("Not yet implemented");
+  }
+
+}

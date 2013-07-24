cf2cca08a8e47ef66b2a88f0441ff815ebd9ea55
James C. Holder <jholder@theladders.com>
2013-07-24 14:38:24 -0400

    Refactor JobInfoComposite to take and use a Compensation object instead of string its own specific instance variables.

--- The-Interface-Segregation-Principle/java/src/main/java/com/theladders/solid/isp/oldjob/JobInfoComposite.java
+++ The-Interface-Segregation-Principle/java/src/main/java/com/theladders/solid/isp/oldjob/JobInfoComposite.java
@@ -1,14 +1,17 @@
 package com.theladders.solid.isp.oldjob;
 
 import com.theladders.solid.isp.oldjob.fulldata.Company;
+import com.theladders.solid.isp.oldjob.fulldata.Compensation;
 
 public class JobInfoComposite extends JobImpl
 {
   private final Company company;
+  private final Compensation compensation;
   
-  public JobInfoComposite(Company company)
+  public JobInfoComposite(Company company, Compensation compensation)
   {
     this.company = company;
+    this.compensation = compensation;
   }
   
   @Override
@@ -21,6 +24,18 @@ public class JobInfoComposite extends JobImpl
   public Integer getCompanySize()
   {
     return company.getSize();
+  }
+  
+  @Override
+  public String getCompensation()
+  {
+    return this.compensation.getDescription();
+  }
+  
+  @Override
+  public String getCompensationBonus()
+  {
+    return this.compensation.getBonus();
   }
 
   @Override

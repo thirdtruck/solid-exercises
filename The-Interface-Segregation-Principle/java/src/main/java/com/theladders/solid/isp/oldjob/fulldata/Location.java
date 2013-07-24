770fe53b9d69b621352287db762e07548c78cc8f
James C. Holder <jholder@theladders.com>
2013-07-24 16:11:48 -0400

    Add Location class.

---
+++ The-Interface-Segregation-Principle/java/src/main/java/com/theladders/solid/isp/oldjob/fulldata/Location.java
@@ -0,0 +1,15 @@
+package com.theladders.solid.isp.oldjob.fulldata;
+
+public class Location {
+  private final String location;
+  
+  public Location(String location)
+  {
+    this.location = location;
+  }
+  
+  public String getLocation()
+  {
+    return location;
+  }
+}

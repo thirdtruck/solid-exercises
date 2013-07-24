3b3ae8646bfdc709cf006b1dad6a12c4c8204def
James C. Holder <jholder@theladders.com>
2013-07-24 09:22:04 -0400

    Remove unused Environment#getString and Environment#getAdminEmail methods.

--- The-Liskov-Substitution-Principle/java/src/main/java/com/theladders/solid/lsp/Environment.java
+++ The-Liskov-Substitution-Principle/java/src/main/java/com/theladders/solid/lsp/Environment.java
@@ -11,23 +11,4 @@ public class Environment extends HashMap<Object, Object>
     super();
   }
 
-  /**
-   * Convenience method that returns the admin email address for this ladder.
-   *
-   * @return email address or "" if either the user or domain is not defined
-   */
-
-  public String getAdminEmail()
-  {
-    String user = getString("admin");
-    String domain = getString(KEY_EMAIL_DOMAIN);
-
-    return user.length() > 0 && domain.length() > 0 ? user + "@" + domain : "";
-  }
-
-  public String getString(String key)
-  {
-    Object val = get(key);
-    return (val != null) ? val.toString().trim() : "";
-  }
 }

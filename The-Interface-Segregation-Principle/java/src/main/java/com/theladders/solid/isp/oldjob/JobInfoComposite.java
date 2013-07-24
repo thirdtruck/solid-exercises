3b3d4a1e67e08db9b55ea2008b844b6d6683f52e
James C. Holder <jholder@theladders.com>
2013-07-24 14:21:19 -0400

    Update JobInfoComposite to take and use a Company instead of storing direct values.

--- The-Interface-Segregation-Principle/java/src/main/java/com/theladders/solid/isp/oldjob/JobInfoComposite.java
+++ The-Interface-Segregation-Principle/java/src/main/java/com/theladders/solid/isp/oldjob/JobInfoComposite.java
@@ -1,9 +1,20 @@
 package com.theladders.solid.isp.oldjob;
 
+import com.theladders.solid.isp.oldjob.fulldata.Company;
+
 public class JobInfoComposite extends JobImpl
 {
-  public JobInfoComposite()
+  private final Company company;
+  
+  public JobInfoComposite(Company company)
   {
+    this.company = company;
+  }
+  
+  @Override
+  public String getCompany()
+  {
+    return company.getName();
   }
 
   @Override

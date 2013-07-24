56d4c65880298c5b50b4506ba6a941f0ab8aeffa
James C. Holder <jholder@theladders.com>
2013-07-24 14:12:12 -0400

    Remove extraneous class import.

--- The-Interface-Segregation-Principle/java/src/test/java/com/theladders/solid/isp/oldjob/TestJobInfoComposite.java
+++ The-Interface-Segregation-Principle/java/src/test/java/com/theladders/solid/isp/oldjob/TestJobInfoComposite.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.theladders.solid.isp.oldjob.JobImpl;
 import com.theladders.solid.isp.oldjob.JobInfoComposite;
 
 

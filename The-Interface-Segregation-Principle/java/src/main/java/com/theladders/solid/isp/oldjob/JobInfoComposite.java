372d71fbc3aa3e65e213db8b2ec1d017cbd71f63
James C. Holder <jholder@theladders.com>
2013-07-24 14:01:29 -0400

    Add JobInfoComposite class, fulfilling the recent tests.

---
+++ The-Interface-Segregation-Principle/java/src/main/java/com/theladders/solid/isp/oldjob/JobInfoComposite.java
@@ -0,0 +1,20 @@
+package com.theladders.solid.isp.oldjob;
+
+public class JobInfoComposite extends JobImpl
+{
+  public JobInfoComposite()
+  {
+  }
+
+  @Override
+  public boolean isJobReq() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public String getDescription() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+}

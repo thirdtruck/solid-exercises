bf944c3cc91f08423f24b525c3237cf2ef7bd0e7
James C. Holder <jholder@theladders.com>
2013-07-25 10:53:56 -0400

    Add AccessibilityController and LocationController placeholder classes, to be used by App.

---
+++ The-Interface-Segregation-Principle/java/src/main/java/com/theladders/solid/isp/oldjob/AccessibilityController.java
@@ -0,0 +1,12 @@
+package com.theladders.solid.isp.oldjob;
+
+import com.theladders.solid.isp.oldjob.fulldata.AccessibilityInfo;
+
+public class AccessibilityController {
+
+  public AccessibilityController(Object accessibilityView,
+      AccessibilityInfo accessibilityInfo) {
+    // TODO Auto-generated constructor stub
+  }
+
+}
---
+++ The-Interface-Segregation-Principle/java/src/main/java/com/theladders/solid/isp/oldjob/LocationController.java
@@ -0,0 +1,11 @@
+package com.theladders.solid.isp.oldjob;
+
+import com.theladders.solid.isp.oldjob.fulldata.LocationInfo;
+
+public class LocationController {
+
+  public LocationController(Object locationView, LocationInfo locationInfo) {
+    // TODO Auto-generated constructor stub
+  }
+
+}

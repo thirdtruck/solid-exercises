fa22876cbbccdfce71e0163b78cf670cc4a29a1e
James C. Holder <jholder@theladders.com>
2013-07-25 10:01:33 -0400

    Add DisciplineListInfo interface.

---
+++ The-Interface-Segregation-Principle/java/src/main/java/com/theladders/solid/isp/oldjob/fulldata/DisciplineListInfo.java
@@ -0,0 +1,8 @@
+package com.theladders.solid.isp.oldjob.fulldata;
+
+import java.util.List;
+import com.theladders.solid.isp.oldjob.stubs.Discipline;
+
+public interface DisciplineListInfo {
+  public List<Discipline> disciplines();
+}

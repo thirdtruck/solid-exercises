fc094897ff3cd5e6edf5ca1a5659eee8229ade26
James C. Holder <jholder@theladders.com>
2013-07-25 10:34:00 -0400

    Add tests for App.

---
+++ The-Interface-Segregation-Principle/java/src/test/java/com/theladders/solid/isp/oldjob/TestApp.java
@@ -0,0 +1,21 @@
+package com.theladders.solid.isp.oldjob;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class TestApp
+{
+
+  @Before
+  public void setUp()
+  {
+  }
+
+  @Test
+  private void thenIShouldBeAbleToRunTheApp()
+  {
+    App.runApp();
+  }
+
+}

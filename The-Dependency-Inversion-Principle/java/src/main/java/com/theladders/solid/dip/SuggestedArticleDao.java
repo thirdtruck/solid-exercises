57fd589131c3c183d6efea4fe0d05ee1ab22a9d2
James C. Holder <jholder@theladders.com>
2013-07-26 10:57:33 -0400

    Update SuggestedArticleDao to actually save articles instead of just always supplying new ones.

--- The-Dependency-Inversion-Principle/java/src/main/java/com/theladders/solid/dip/SuggestedArticleDao.java
+++ The-Dependency-Inversion-Principle/java/src/main/java/com/theladders/solid/dip/SuggestedArticleDao.java
@@ -1,19 +1,26 @@
 package com.theladders.solid.dip;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class SuggestedArticleDao
 {
+  private Map<Integer, SuggestedArticle> articlesBySubscriberId;
+  
+  public SuggestedArticleDao()
+  {
+    articlesBySubscriberId = new HashMap<Integer, SuggestedArticle>();
+  }
+  
   public void updateByPrimaryKeySelective(@SuppressWarnings("unused") SuggestedArticle article) {}
 
   public int insertReturnId(@SuppressWarnings("unused") SuggestedArticle suggestedArticle)
   {
+    articlesBySubscriberId.put(suggestedArticle.getSubscriberId(), suggestedArticle);
     return 0;
   }
 
   public List<SuggestedArticle> selectByExampleWithBlobs(@SuppressWarnings("unused") SuggestedArticleExample criteria)
   {
-    return Collections.singletonList(new SuggestedArticle());
+    return new ArrayList<SuggestedArticle>(articlesBySubscriberId.values());
   }
 }

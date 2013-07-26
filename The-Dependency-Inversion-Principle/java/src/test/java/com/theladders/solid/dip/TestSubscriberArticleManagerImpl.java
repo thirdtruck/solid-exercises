87664b0c7249b9629d25d09f26d954e168e660cb
James C. Holder <jholder@theladders.com>
2013-07-26 10:25:29 -0400

    Add basic test files.

---
+++ The-Dependency-Inversion-Principle/java/src/test/java/com/theladders/solid/dip/TestSubscriberArticleManagerImpl.java
@@ -0,0 +1,22 @@
+package com.theladders.solid.dip;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestSubscriberArticleManagerImpl
+{
+
+  @Before
+  public void setUp()
+  {
+  }
+
+  @Test
+  public void test()
+  {
+    fail("Not yet implemented");
+  }
+
+}
